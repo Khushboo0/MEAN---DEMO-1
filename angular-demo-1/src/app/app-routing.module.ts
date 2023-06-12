94cde748b0885fdcc1e537ef3f8fc1fd813e9d60
Khushboo <patelkhushboo057@gmail.com>
2023-06-13 00:58:48 +0530

    added header,footer,home component, services

--- angular-demo-1/src/app/app-routing.module.ts
+++ angular-demo-1/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+// import { HeaderComponent } from './header/header.component';
+// HomeComponent
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full'},
+{ path: 'Home', component: HomeComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
